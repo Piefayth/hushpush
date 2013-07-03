bf85155778d782e9a253d226d4e201cec1b13610
Sean C. Ryan <piefayth@gmail.com>
2013-07-03 16:21:02 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Hushpush::Application.routes.draw do
-  resources :users
 
   root to: 'static_pages#home'
+  
+  resources :users, only: [:new, :create, :destroy]
 
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  match 'signup', to: 'users#new'
-
+  match '/signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
