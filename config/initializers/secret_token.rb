2a85ecf8bef1dd6451466f2e6b22678595df1aaa
Sean C. Ryan <piefayth@gmail.com>
2013-07-03 13:39:31 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hushpush::Application.config.secret_token = 'b5e9dac795084218c56617049094afca71f9db4d5f7f02ffdbb134c654b1669a6bafecc6007059fcaef1da33770652967cbb7b3fbfdc8bf6cace58228e999806'
