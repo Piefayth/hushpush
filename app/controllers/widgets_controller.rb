b8abcc9207474fe7f199e404659a79bf20216fed
Sean C. Ryan <piefayth@gmail.com>
2013-07-06 15:42:36 -0400

    Finished weather widget

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,5 +1,6 @@
 class WidgetsController < ApplicationController
 	before_filter :signed_in_user
+	before_filter :correct_user, only: [:destroy, :update]
 
 	def create
 		@widget = current_user.widgets.build(params[:widget])
@@ -26,5 +27,12 @@ class WidgetsController < ApplicationController
 		@widget.properties = params.except(:utf8, :_method, :authenticity_token, :commit, :action, :controller, :id).as_json
 		@widget.save
 		redirect_to root_url
-	end
+	end		
+
+	private
+
+		def correct_user
+			@widget = current_user.widgets.find_by_id(params[:id])
+			redirect_to root_url if @widget.nil?
+		end
 end
--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -1,7 +1,7 @@
 module WidgetsHelper
 
 	def widget_types
-		return ['Clock', 'Weather', 'RSS']
+		return ['Clock', 'Weather', 'RSS', 'ToDo', 'Email', 'Calendar', 'Facebook', 'Twitter']
 	end
 
 	def widget_exists?(widget)
@@ -12,16 +12,30 @@ module WidgetsHelper
 		{}
 	end
 
-	def get_weather(widget)
+	def get_current_weather(widget)
 		unless Rails.cache.fetch("weather#{widget.id}").nil?
 			return Rails.cache.fetch("weather#{widget.id}")
 		else
 			require 'net/http'
 			city = widget.properties["city"]
 			country = widget.properties["country"]
-			url = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{city},#{country}&cnt=1&mode=json&APPID=0a0cb68421eaf931257479eaf5a8a29c".to_s)
+			url = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{city},#{country}&cnt=1&units=imperial&mode=json&APPID=0a0cb68421eaf931257479eaf5a8a29c".to_s)
 			res = Net::HTTP.get_response(url)
 			Rails.cache.write("weather#{widget.id}", res.body, expires_in: 10.minutes)
+			return res.body
+		end
+	end
+
+	def get_forecast_weather(widget)
+		unless Rails.cache.fetch("fweather#{widget.id}").nil?
+			return Rails.cache.fetch("fweather#{widget.id}")
+		else
+			require 'net/http'
+			city = widget.properties["city"]
+			country = widget.properties["country"]
+			url = URI.parse("http://api.openweathermap.org/data/2.5/forecast?q=#{city},#{country}&cnt=1&units=imperial&mode=json&APPID=0a0cb68421eaf931257479eaf5a8a29c".to_s)
+			res = Net::HTTP.get_response(url)
+			Rails.cache.write("fweather#{widget.id}", res.body, expires_in: 60.minutes)
 			return res.body
 		end
 	end
