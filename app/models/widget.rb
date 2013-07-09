e12170985166835f2f80c9fbfa54e2a60e57db05
Sean C. Ryan <piefayth@gmail.com>
2013-07-09 17:12:17 -0400

    Fixed e-mail issues.

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -13,7 +13,7 @@ class Widget < ActiveRecord::Base
 		require 'mail'
 
 		imap = Net::IMAP.new('imap.gmail.com', 993, usessl = true, certs = nil, verify = false)
-		if self.updated_at + self.properties["gmail_expires_in"] - 5 < Time.now
+		if self.updated_at + self.properties["gmail_expires_in"] - 5 < Time.now || self.properties["gmail_access_token"].blank?
 			refresh_gmail_access_token
 		end
 		begin
@@ -26,11 +26,9 @@ class Widget < ActiveRecord::Base
 			imap.disconnect
 			return messages
 		rescue
-			#self.properties["gmail_access_token"] = " "
-			#self.properties["gmail_refresh_token"] = " "
-			#self.properties["gmail_expires_in"] = " "
-			return nil
+			self.properties["gmail_access_token"] = " "
 			self.save
+			get_gmail
 		end
 	end
 
@@ -38,14 +36,13 @@ class Widget < ActiveRecord::Base
 		def refresh_gmail_access_token
 			url = URI.parse('https://accounts.google.com/o/oauth2/token')
 			request = Net::HTTP::Post.new(url.request_uri)
-			request.set_form_data({refresh_token: self.properties["refresh_token"], 
+			request.set_form_data({refresh_token: self.properties["gmail_refresh_token"], 
 									client_id: '948359240643.apps.googleusercontent.com',
 									client_secret: 'KlDdcCN1lnetPu2eL1fzI3rf',
 									grant_type: 'refresh_token'})
 			response = Net::HTTP.start(url.host, use_ssl: true) do |http| http.request(request) end
 			response = JSON.parse(response.body)
-
-			self.properties["access_token"] = response["access_token"]
+			self.properties["gmail_access_token"] = response["access_token"]
 			self.properties["gmail_expires_in"] = response["expires_in"]
 			self.save
 		end
